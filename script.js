dc78bf94bdcbf4e8a12bce247aa95daa526fa8f4
Pedro Henrique <pedrogregolini@gmail.com>
2025-04-19 13:18:11 -0300

    Questão - 8

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 //Questão - 1
 
 //Valor Undefined
-let exemplo;
-console.log(exemplo)
+// let exemplo;
+// console.log(exemplo)
 
 //Valor Nulo
-let exemplo1 = null;
-console.log(exemplo1)
+// let exemplo1 = null;
+// console.log(exemplo1)
 
 //Com valor
-let exemplo2 = 1;
-console.log(exemplo2)
+// let exemplo2 = 1;
+// console.log(exemplo2)
 
+//Questão - 8
+const texto = "Tecnologia é um produto da ciência e da engenharia que envolve um conjunto de instrumentos, métodos e técnicas que visam a resolução de problemas"
+const em = texto.indexOf("em")
+const ia = texto.lastIndexOf("ia")
+const ciencia = texto.includes("ciência")
+const metodos = texto.includes("método")
+
+console.log("Primeira posição de 'em'", em)
+console.log("Última posição de 'ia'", ia)
+console.log("Palavra 'Ciência' encontrada?", ciencia)
+console.log("Palavra 'métodos' encontrada?", metodos)
