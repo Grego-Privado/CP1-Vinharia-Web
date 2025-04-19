2acb0012c100ddffb0fbed5c98969b49f398b65c
Pedro Henrique <pedrogregolini@gmail.com>
2025-04-19 13:23:04 -0300

    Questão - 9

--- script.js
+++ script.js
@@ -13,13 +13,19 @@
 // console.log(exemplo2)
 
 //Questão - 8
-const texto = "Tecnologia é um produto da ciência e da engenharia que envolve um conjunto de instrumentos, métodos e técnicas que visam a resolução de problemas"
-const em = texto.indexOf("em")
-const ia = texto.lastIndexOf("ia")
-const ciencia = texto.includes("ciência")
-const metodos = texto.includes("método")
+// const texto = "Tecnologia é um produto da ciência e da engenharia que envolve um conjunto de instrumentos, métodos e técnicas que visam a resolução de problemas"
+// const em = texto.indexOf("em")
+// const ia = texto.lastIndexOf("ia")
+// const ciencia = texto.includes("ciência")
+// const metodos = texto.includes("método")
 
-console.log("Primeira posição de 'em'", em)
-console.log("Última posição de 'ia'", ia)
-console.log("Palavra 'Ciência' encontrada?", ciencia)
-console.log("Palavra 'métodos' encontrada?", metodos)
+// console.log("Primeira posição de 'em'", em)
+// console.log("Última posição de 'ia'", ia)
+// console.log("Palavra 'Ciência' encontrada?", ciencia)
+// console.log("Palavra 'métodos' encontrada?", metodos)
+
+//Questão - 9
+let variavel = "1.23"
+variavel = parseFloat(variavel)
+console.log(variavel)
+console.log(typeof variavel)
