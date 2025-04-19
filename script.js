86a8561d672d239aa672864dedccedd6715b3d55
Pedro Henrique <pedrogregolini@gmail.com>
2025-04-19 13:11:48 -0300

    Exercicio 1

---
+++ script.js
@@ -0,0 +1,14 @@
+//Questão - 1
+
+//Valor Undefined
+let exemplo;
+console.log(exemplo)
+
+//Valor Nulo
+let exemplo1 = null;
+console.log(exemplo1)
+
+//Com valor
+let exemplo2 = 1;
+console.log(exemplo2)
+
